fcc3a230225e416b8bb19898b08fe8299631d5d1
mismathh <mismathhimam@myseneca.ca>
2023-11-15 13:43:49 -0500

    Test CI workflow by breaking logic in function

--- src/parseCodeBlock.js
+++ src/parseCodeBlock.js
@@ -5,7 +5,7 @@ const parseCodeBlock = (lines) => {
 
   let html = lines;
 
-  const codeBlockPattern = /```([^*]+)```/g;
+  const codeBlockPattern = /```([^*]+)``break`/g;
   html = html.replace(codeBlockPattern, (match, codeText) => {
     codeBlock = `<pre>\n\t\t\t\t<code>${codeText}\n\t\t\t\t</code>\n\t\t\t</pre>`;
     return codeBlock;
